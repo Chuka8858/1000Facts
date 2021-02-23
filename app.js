0e1771196f9d797ae7367b5d2e362aeb1f85c86d
Chuka8858 <77659851+Chuka8858@users.noreply.github.com>
2021-02-23 17:51:02 +0800

    hamss
    burger

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const menu = document.querySelector('#mobile-menu');
-// const menuLinks = document.querySelector('.navbar__menu');
-// const navLogo = document.querySelector('#navbar__logo');
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
 // Display Mobile Menu
 const mobileMenu = () => {
